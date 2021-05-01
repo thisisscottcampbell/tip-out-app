e7e5b125b72f8a4feb419a10087c8f89e549083d
Cara Dibdin <cdibdin@gmail.com>
2021-05-01 16:54:05 -0400

    client query example
    Co-authored-by: Giovanni Lituma <giovanninet@icloud.com>

---
+++ server/clientqueries.js
@@ -0,0 +1,17 @@
+mutation signUpUser(
+    $first_name: String
+        $last_name: String
+        $email: String
+        $password: String
+        $position: String
+        $admin: Boolean
+        $company: String
+  ) {
+    signUpUser(first_name: $first_name, last_name: $last_name, email: $email, password: $password, position: $position, admin: $admin, company: $company) {
+      first_name
+      last_name
+      email
+    }
+  }
+
+  {"first_name": "cara", "last_name": "dibdin", "email": "dibdin@gmail.com", "password": "1234", "position": "123", "admin": true, "company": "Logans restaurant"}
