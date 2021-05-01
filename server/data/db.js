fa427f7a2e412abb53fa43662605c8735245aa22
Cara Dibdin <cdibdin@gmail.com>
2021-05-01 14:09:52 -0400

    database setup
    Co-authored-by: giovannixdev <giovanninet@icloud.com>

---
+++ server/data/db.js
@@ -0,0 +1,9 @@
+const knex = require("knex");
+require("dotenv").config();
+
+const knexfile = require("../knexfile");
+
+const env = process.env.NODE_ENV || "development";
+const configOptions = knexfile[env];
+
+module.exports = knex(configOptions);
