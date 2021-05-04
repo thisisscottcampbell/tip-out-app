9b66b7325e77b85617e8a835c86e4eabe4838a45
Cara Dibdin <cdibdin@gmail.com>
2021-05-03 22:34:01 -0400

    add findUser and signIn controller middleware
    Co-authored-by: Giovanni Lituma <giovanninet@icloud.com>

--- server/controllers/userscontrollers.js
+++ server/controllers/userscontrollers.js
@@ -35,7 +35,7 @@ const createUser = (user) => {
         user.last_name,
         user.email,
         user.password,
-        user.position,
+        "66330321-a3d4-4ac2-8067-827b216cb7aa", //user.position
         user.admin,
         user.company,
       ]
@@ -43,6 +43,52 @@ const createUser = (user) => {
     .then((data) => data.rows[0]);
 };
 
+const findUser = (userEmail) => {
+  return db
+    .raw("SELECT * FROM users WHERE email = ?", [userEmail])
+    .then((data) => {
+      return data.rows[0];
+    });
+};
+
+const signInUser = (userRequest) => {
+  //userRequest is an object with both the email and password
+  let user;
+  return findUser(userRequest.email)
+    .then((foundUser) => {
+      user = foundUser;
+      return checkPassword(userRequest.password, foundUser);
+    })
+    .then(
+      () => {
+        return user;
+      },
+      (err) => {
+        return "User not found, please verify the fields";
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+      return "Cannot sign in, please contact admin";
+    });
+};
+
+const checkPassword = (reqPassword, foundUser) => {
+  return new Promise((resolve, reject) =>
+    bcrypt.compare(reqPassword, foundUser.password, (err, response) => {
+      if (err) {
+        reject(err);
+      } else if (response) {
+        resolve(response);
+      } else {
+        reject(new Error("Verify your password"));
+      }
+    })
+  );
+};
+
 module.exports = {
   signUp,
+  signInUser,
+  findUser,
 };
