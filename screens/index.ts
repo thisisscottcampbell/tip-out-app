34ab4fbf147418d8c8b692ef06982d5302338935
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-05-03 22:08:08 -0400

    Added Login and Dashboard

--- screens/index.ts
+++ screens/index.ts
@@ -1,6 +1,8 @@
 //import HomeScreen from "./HomeScreen/HomeScreen";
 
-export * from './HomeScreen'
+export * from './WelcomeScreen'
 export * from './NotFoundScreen'
 export * from './TabOneScreen'
 export * from './TabTwoScreen'
+export * from './LoginScreen'
+export * from './DashboardScreen'
