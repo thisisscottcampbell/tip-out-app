63a6eec7314ee6161d906e8bf34f78d6fa50a166
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-05-03 22:06:06 -0400

    Added DashboardScreen + index + styles

---
+++ screens/DashboardScreen/DashboardScreen.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import styles from "./styles";
+import { Text, View } from "react-native";
+
+export default function DashboardScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard</Text>
+
+
+    </View>
+  );
+}
