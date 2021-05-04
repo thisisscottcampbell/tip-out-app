c5c6f05096afe494357fdd5401b4fe63efeffd3c
Logan Thies <logan.thies@icloud.com>
2021-05-04 00:09:59 -0700

    remove unneccessary screens

--- screens/index.ts
+++ screens/index.ts
@@ -1,8 +1,6 @@
 //import HomeScreen from "./HomeScreen/HomeScreen";
 
-export * from './WelcomeScreen'
-export * from './NotFoundScreen'
-export * from './TabOneScreen'
-export * from './TabTwoScreen'
-export * from './LoginScreen'
-export * from './DashboardScreen'
+export * from "./WelcomeScreen";
+export * from "./NotFoundScreen";
+export * from "./LoginScreen";
+export * from "./HomeScreen";
