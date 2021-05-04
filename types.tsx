3cc5ddd8403806e127c7e19457d8d79a8e15cb9b
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-05-03 22:08:30 -0400

    Added NavigationPropTypes

--- types.tsx
+++ types.tsx
@@ -3,9 +3,32 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
+ import { StackNavigationProp } from '@react-navigation/stack';
+
+ export type LoginScreenNavigationProp = StackNavigationProp<
+   RootStackParamList,
+   'Login'
+ >;
+ 
+ export type LoginProps = {
+   navigation: LoginScreenNavigationProp;
+ };
+
+ export type WelcomeScreenNavigationProp = StackNavigationProp<
+ RootStackParamList,
+ 'Welcome'
+>;
+
+export type WelcomeProps = {
+ navigation: LoginScreenNavigationProp;
+};
+
 export type RootStackParamList = {
   Root: undefined;
+  Login: undefined;
   NotFound: undefined;
+  BottomTab: undefined;
+  Welcome: undefined;
 };
 
 export type BottomTabParamList = {
