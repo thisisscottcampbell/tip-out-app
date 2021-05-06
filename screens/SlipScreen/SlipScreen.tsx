f77b2ef0f9b4a7f3f35f31cae78667a6c420d89f
Logan Thies <logan.thies@icloud.com>
2021-05-05 23:39:26 -0700

    remove DropDown component, replace w/ tipOutRow

--- screens/SlipScreen/SlipScreen.tsx
+++ screens/SlipScreen/SlipScreen.tsx
@@ -2,16 +2,20 @@ import * as React from "react";
 import styles from "./styles";
 // import { useForm, Controller } from "react-hook-form";
 import { Text, View } from "react-native";
+import TipOutRow from "../../components/TipOutRow";
 import DropDown from "../../components/DropDown";
 import StyledPrimaryButton from "../../components/PrimaryButton";
 import { SlipProps } from "../../types";
 
-export default function SlipScreen({navigation}: SlipProps) {
+export default function SlipScreen({ navigation }: SlipProps) {
   return (
     <View style={styles.container}>
       <Text>Slip</Text>
-      {/* <DropDown /> */}
-      <StyledPrimaryButton text={"Submit"} onPress={() => navigation.navigate('ReviewScreen')} />
+      <TipOutRow />
+      <StyledPrimaryButton
+        text={"Submit"}
+        onPress={() => navigation.navigate("ReviewScreen")}
+      />
     </View>
   );
 }
