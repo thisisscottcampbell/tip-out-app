f66d65fd896e98566d5e90649805a79f32b3be67
Logan Thies <logan.thies@icloud.com>
2021-05-05 17:08:56 -0700

    Create Dropdown Component

--- screens/SlipScreen/SlipScreen.tsx
+++ screens/SlipScreen/SlipScreen.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import styles from "./styles";
-import { useForm, Controller } from "react-hook-form";
-import { Text, View, TextInput, Alert } from "react-native";
-import StyledPrimaryButton from "../../components/PrimaryButton";
+// import { useForm, Controller } from "react-hook-form";
+import { Text, View } from "react-native";
+import DropDown from "../../components/DropDown";
 
 export default function SlipScreen() {
   return (
     <View style={styles.container}>
       <Text>Slip</Text>
+      <DropDown />
     </View>
   );
 }
