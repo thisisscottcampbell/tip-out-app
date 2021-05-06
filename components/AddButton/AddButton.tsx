709a7598776dbf679787c939ff3128d993b2b596
Logan Thies <logan.thies@icloud.com>
2021-05-05 23:37:42 -0700

    create add button component for tipout slip

---
+++ components/AddButton/AddButton.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styles from "./styles";
+import { Text, TouchableOpacity } from "react-native";
+
+export default function AddButton(props: any) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={props.onPress}>
+      <Text style={styles.text}>{props.text}</Text>
+    </TouchableOpacity>
+  );
+}
