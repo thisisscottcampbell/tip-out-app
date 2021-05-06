537657bbdb22ff8aa37dcd1120f379234888d89b
Logan Thies <logan.thies@icloud.com>
2021-05-05 23:38:59 -0700

    create TipOutRow component -> change to Input

---
+++ components/TipOutRow/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fae6ff",
+    borderWidth: 1,
+    borderColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15,
+  },
+  input: {
+    margin: 10,
+    width: 85,
+    height: 50,
+    fontSize: 25,
+    color: "#b800e6",
+    backgroundColor: "white",
+    textAlign: "center",
+  },
+});
+
+export default styles;
