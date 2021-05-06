faa59d9c85ad247320d66e253c52a7f011edf4c1
Logan Thies <logan.thies@icloud.com>
2021-05-05 23:40:01 -0700

    mock data for employees

---
+++ Data/Employees.js
@@ -0,0 +1,32 @@
+export default employees = [
+  {
+    id: 0,
+    firstName: "Logan",
+    lastName: "Thies",
+    position: "server",
+  },
+  {
+    id: 1,
+    firstName: "Konrad",
+    lastName: "Kopko",
+    position: "bartender",
+  },
+  {
+    id: 2,
+    firstName: "Cara",
+    lastName: "Dibdin",
+    position: "host",
+  },
+  {
+    id: 3,
+    firstName: "Giovanni",
+    lastName: "Lithuma",
+    position: "bartender",
+  },
+  {
+    id: 4,
+    firstName: "Scott",
+    lastName: "Campbell",
+    position: "Busser",
+  },
+];
