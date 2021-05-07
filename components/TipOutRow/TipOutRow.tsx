0d4556875001dab7160978bf6477191dd4fe98b2
Logan Thies <logan.thies@icloud.com>
2021-05-07 11:27:02 -0700

    refactor

--- components/TipOutRow/TipOutRow.tsx
+++ components/TipOutRow/TipOutRow.tsx
@@ -8,23 +8,18 @@ import transactions from "../../Data/Transactions";
 import employees from "../../Data/Employees";
 
 export default function TipOutRow({ slip }: any) {
-  console.log("tansactions --> ", transactions);
-
-  const test = transactions.filter((transaction) => {
-    console.log("slip trans --> ", slip.transaction);
-    console.log("trans.id --> ", transaction.id);
+  const transaction = transactions.filter((transaction) => {
     return slip.transaction === transaction.id;
   });
 
-  console.log("test --> ", test);
-  console.log("test.from --> ", test[0].from);
-  console.log("test.to --> ", test[0].to);
+  const employeeFrom = employees[transaction[0].from];
+  const employeeTo = employees[transaction[0].to];
 
   return (
     <View style={{ flexDirection: "row" }}>
-      <Text>{`From: ${employees[test[0].from].firstName} |`}</Text>
-      <Text>{`To: ${employees[test[0].to].lastName} |`}</Text>
-      <Text>{`Amount: ${slip.amount}`}</Text>
+      {/* <Text style={styles.text}>{employeeFrom.firstName}</Text> */}
+      <Text style={styles.text}>{employeeTo.firstName}</Text>
+      <Text style={styles.text}>{`$${slip.amount}`}</Text>
     </View>
   );
 }
