9c65ec5c9f86e31e1f04c89ee3a53d2a0fa2bbbb
Logan Thies <logan.thies@icloud.com>
2021-05-07 11:23:51 -0700

    merging props styles with component styles

--- components/AddButton/AddButton.tsx
+++ components/AddButton/AddButton.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import styles from "./styles";
 import { Text, TouchableOpacity } from "react-native";
 
-export default function AddButton(props: any) {
+export default function AddButton(props) {
   return (
-    <TouchableOpacity style={styles.button} onPress={props.onPress}>
+    <TouchableOpacity
+      style={{ ...styles.button, ...props.style }}
+      onPress={props.onPress}
+    >
       <Text style={styles.text}>{props.text}</Text>
     </TouchableOpacity>
   );
