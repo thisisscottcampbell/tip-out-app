f707aaf85aaf305b16502e1c8db5f718c777d5aa
Logan Thies <logan.thies@icloud.com>
2021-05-07 11:27:27 -0700

    refactor

--- screens/SlipScreen/SlipScreen.tsx
+++ screens/SlipScreen/SlipScreen.tsx
@@ -3,12 +3,9 @@ import styles from "./styles";
 // import { useForm, Controller } from "react-hook-form";
 import { Text, View } from "react-native";
 import TipOutInput from "../../components/TipOutInput";
-import TipOutRow from "../../components/TipOutRow";
 import DropDown from "../../components/DropDown";
 import StyledPrimaryButton from "../../components/PrimaryButton";
 import { SlipProps } from "../../types";
-
-import slips from "../../Data/Slips";
 
 export default function SlipScreen({ navigation }: SlipProps) {
   //containerize slips
@@ -16,10 +13,7 @@ export default function SlipScreen({ navigation }: SlipProps) {
     <View style={styles.container}>
       <Text>Slip</Text>
       <TipOutInput />
-      {/* {slips.map((slip) => (
-        <TipOutRow slip={slip} />
-      ))} */}
-      <TipOutRow slip={slips[0]} />
+
       <StyledPrimaryButton
         text={"Submit"}
         onPress={() => navigation.navigate("ReviewScreen")}
