f0da0d8f83a9e156a10b1463ac31cfb37fd9a8d5
Giovanni Lituma <giovanninet@icloud.com>
2021-05-06 23:04:50 -0400

    add /react to @apollo/client dependencie

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ApolloProvider } from "@apollo/client"
+import { ApolloProvider } from "@apollo/client/react"
 import React from "react";
 import { client } from "./graphql/client"
 import { Provider } from "react-redux";
