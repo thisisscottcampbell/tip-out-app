1be2988281e2ecf263c4b489046ce21e20246c9e
Logan Thies <logan.thies@icloud.com>
2021-05-08 09:51:51 -0700

    Add confirmation data and response

--- screens/ConfirmScreen/ConfirmScreen.tsx
+++ screens/ConfirmScreen/ConfirmScreen.tsx
@@ -2,14 +2,30 @@ import * as React from "react";
 import styles from "./styles";
 // import { useForm, Controller } from "react-hook-form";
 import { Text, View } from "react-native";
-import StyledPrimaryButton from "../../components/PrimaryButton";
-import { ConfirmProps } from "../../types";
+import { RootState } from "../../state/store";
+import { useSelector } from "react-redux";
 
-export default function ConfirmScreen({navigation}: ConfirmProps) {
+export default function ConfirmScreen() {
+  const { shiftData, slips } = useSelector(
+    (state: RootState) => state.slipReducer
+  );
+
+  const total = slips.reduce((total, slip) => {
+    total += slip.amount;
+    return total;
+  }, 0);
+
+  const { tipsReceived } = shiftData;
+  const date = new Date().toString();
+
   return (
     <View style={styles.container}>
-      <Text>Confirm</Text>
-      <StyledPrimaryButton text={"Return Home"} onPress={() => console.log("Returned Home")} />
+      <Text>**Confirmation Graphic Here**</Text>
+      <Text>{new Date().toString()}</Text>
+      <Text>Total tips received {tipsReceived}</Text>
+      <Text>Total tip out {total}</Text>
+      <Text>Total tip out {(total / tipsReceived) * 100}%</Text>
+      <Text>Confirmation Code (table ID for the history for reference?)</Text>
     </View>
   );
 }
