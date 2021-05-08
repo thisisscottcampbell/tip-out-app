5e83dd184aa55fd6ad65efe6836a6b7f0b42739e
Giovanni Lituma <giovanninet@icloud.com>
2021-05-08 03:06:10 -0400

    Add SlipReducer * Action setShiftData where the payload is totalSales, tipsReceived, foodSales, liquorSales and we set these values to the store * Action addSlip where the payload is employeeTo and amount and we add those values and override previous amount for employeer who are selected again

---
+++ state/actions/index.js
@@ -0,0 +1,20 @@
+import * as Types from "../constants/actionTypes";
+
+export const setEmail = (email) => ({
+  type: Types.SET_EMAIL,
+  payload: { email },
+});
+export const setPassword = (password) => ({
+  type: Types.SET_PASSWORD,
+  payload: { password },
+});
+export const addSlip = ({employeeTo,amount}) => ({
+  type: Types.ADD_SLIP,
+  payload: {employeeTo,amount},
+});
+
+export const setShiftData = ({totalSales, tipsReceived, foodSales, liquorSales}) => ({
+  type: Types.SET_SHIFT_DATA,
+  payload: {totalSales, tipsReceived, foodSales, liquorSales},
+});
+
