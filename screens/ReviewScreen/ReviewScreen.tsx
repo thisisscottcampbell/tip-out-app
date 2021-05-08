65fbff4a34346161ce1730f26d88bc297be6636e
Giovanni Lituma <giovanninet@icloud.com>
2021-05-08 03:28:18 -0400

    Review Screen * Render TipOutRowContainer componet * Get tipsReceived amount from redux store using useSelector * Calculating total tip out and percentages
    Co-authored-by: Logan Thies <Thiesl137@users.noreply.github.com>

--- screens/ReviewScreen/ReviewScreen.tsx
+++ screens/ReviewScreen/ReviewScreen.tsx
@@ -4,11 +4,28 @@ import styles from "./styles";
 import { Text, View } from "react-native";
 import StyledPrimaryButton from "../../components/PrimaryButton";
 import { ReviewProps } from "../../types";
+import { RootState } from "../../state/store";
+import { useSelector } from "react-redux";
+import TipOutRowContainer from "../../containers/TipOutRowContainer";
 
 export default function ReviewScreen({navigation}: ReviewProps) {
+  const {shiftData, slips} = useSelector(
+    (state: RootState) => state.slipReducer
+  );
+  const {tipsReceived} = shiftData
+
+  const total = slips.reduce((total, slip) => {
+    total += slip.amount;
+    return total
+  },0)
+  
   return (
     <View style={styles.container}>
       <Text>Review</Text>
+      <TipOutRowContainer />
+      <Text>Total tips received {tipsReceived }</Text>
+      <Text>Total tip out {total}</Text>
+      <Text>Total tip out {total / tipsReceived * 100}%</Text>
       <StyledPrimaryButton text={"Edit"} onPress={() => navigation.goBack()} />
       <StyledPrimaryButton text={"Submit"} onPress={() => navigation.navigate("ConfirmScreen")} />
     </View>
